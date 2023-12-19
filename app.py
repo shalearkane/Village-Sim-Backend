b3e6945b543f230544e12dc0da6bec4ab4717790
Soumik Dutta <78898665+shalearkane@users.noreply.github.com>
2023-12-19 19:10:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ def get_facilities():
 @app.route("/roads", methods=["GET"])
 def get_roads():
     args = request.args
-    north = args.get("", default=28.58, type=float)
-    south = args.get("", default=(north - 0.12), type=float)
-    east = args.get("", default=77.72, type=float)
-    west = args.get("", default=(east - 0.12), type=float)
+    north = args.get("north", default=28.58, type=float)
+    south = args.get("south", default=(north - 0.12), type=float)
+    east = args.get("east", default=77.72, type=float)
+    west = args.get("west", default=(east + 0.12), type=float)
 
     geojson = fetch_roads_geojson(north, south, east, west)
     return clean_roads_data(geojson)
